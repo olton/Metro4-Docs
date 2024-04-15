5441a25a7061ab8d4259e92f4598bb967091adaf
olton <serhii@pimenov.com.ua>
2024-04-15 22:17:57 +0300

    upd icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,35 +71,10 @@ module.exports = function(grunt) {
                             match: /<!-- buy-me-coffee -->/g,
                             replacement: '<div class="text-center mt-8"><a href="https://www.buymeacoffee.com/pimenov"><img src="images/buy-me-coffee.png" alt="Buy me a coffee"></a></div>'
                         },
-                        // {
-                        //     match: /<!-- umami -->/g,
-                        //     replacement: `<script async defer data-website-id="095e47f0-c18a-4c8f-a29f-c96562032f3a" src="https://umami-jade.vercel.app/umami.js"></script>`
-                        // },
                         {
                             match: /<!-- umami -->/g,
-                            replacement: `
-    <!-- hit.ua -->
-    <a href='http://hit.ua/?x=37262' target='_blank'>
-    <script language="javascript" type="text/javascript"><!--
-    Cd=document;Cr="&"+Math.random();Cp="&s=1";
-    Cd.cookie="b=b";if(Cd.cookie)Cp+="&c=1";
-    Cp+="&t="+(new Date()).getTimezoneOffset();
-    if(self!=top)Cp+="&f=1";
-    //--></script>
-    <script language="javascript1.1" type="text/javascript"><!--
-    if(navigator.javaEnabled())Cp+="&j=1";
-    //--></script>
-    <script language="javascript1.2" type="text/javascript"><!--
-    if(typeof(screen)!='undefined')Cp+="&w="+screen.width+"&h="+
-    screen.height+"&d="+(screen.colorDepth?screen.colorDepth:screen.pixelDepth);
-    //--></script>
-    <script language="javascript" type="text/javascript"><!--
-    Cd.write("<sc"+"ript src='//c.hit.ua/hit?i=37262&g=0&x=3"+Cp+Cr+
-    "&r="+escape(Cd.referrer)+"&u="+escape(window.location.href)+"'></sc"+"ript>");
-    //--></script></a>
-    <!-- / hit.ua -->                            
-                            `
-                        }
+                            replacement: `<script src="https://cdn.jsdelivr.net/npm/@docsearch/js@3" onload="docsearch({appId: '90CHSLCJTV',apiKey: 'c0207161474ce182d44c10f16181304e',indexName: 'metroui-org',container: '#algolia-search',debug: false});"></script>`
+                        },
                     ]
                 },
                 files: [
